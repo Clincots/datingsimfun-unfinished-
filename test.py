566ab0ed21fcbeb3762d6dbeb7844526f5542cf4
Clincots <79501074+Clincots@users.noreply.github.com>
2022-09-03 20:12:02 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,36 @@
+import time
+import asyncio
+from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import time
-from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
 from kivy.core.window import Window
+from kivy.uix.screenmanager import FadeTransition
+from kivy.clock import Clock
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
-
-Window.fullscreen = True
 Builder.load_file("test.kv")
 
-test123 = ""
 
 class FirstScreen(Screen):
     pass
 
+
 class SecondScreen(Screen):
     pass
 
+
 class ThirdScreen(Screen):
     pass
+
 
 class FourthScreen(Screen):
     pass
 
 
 class CandLApp(App):
-    global test123
+
     def build(self):
         sm = ScreenManager()
         sm.add_widget(FirstScreen(name="first"))
@@ -34,14 +38,25 @@ class CandLApp(App):
         sm.add_widget(ThirdScreen(name="third"))
         sm.add_widget(FourthScreen(name="fourth"))
         return sm
-    def text_wait(self, text):
+
+    def text_wait(self, text, idforbutton, screennumber):
+
+        anything = getattr(self.root.get_screen(screennumber).ids, idforbutton)
+        anything.text = ""
         for i in text:
-            print(self.ids.buttonfortext.text)
-            time.sleep(1)
+            anything.text += i
+            print(anything.text)
+            #Clock.schedule_once(self.dosmth, 1)
+
+    def testingsidk(self, dt):
+        print("cheese")
+
+    def fullscreen(self):
+        Window.fullscreen = True
 
 
-
-
+    def notfullscreen(self):
+        Window.fullscreen = False
 
 if __name__ == "__main__":
     CandLApp().run()
